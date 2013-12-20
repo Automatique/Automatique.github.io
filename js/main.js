a3aea5029741599fc1bac92b6161939a1c58d36c
Tijs Teulings <tijs@automatique.nl>
2013-12-20 14:25:55 +0100

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+
+	$('#slider1').bxSlider({
+	  autoStart: false,
+	  nextText: '&#8594;',
+	  prevText: '&#8592;'
+	});
+	$('#slider2').bxSlider({
+	  autoStart: false,
+	  nextText: '&#8594;',
+	  prevText: '&#8592;'
+	});
+	$('#slider3').bxSlider({
+		autoStart: false,
+		nextText: '&#8594;',
+		prevText: '&#8592;'
+	});
+	$('#slider4').bxSlider({
+		infiniteLoop: true,
+	  	autoStart: false,
+	  	nextText: '&#8594;',
+	  	prevText: '&#8592;'
+	});
+
+});
